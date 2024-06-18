cca2225026bc1cd99afdadeebdd499b5e4438a02
Suyash Bhiste <suyashbhiste0@gmail.com>
2024-06-18 22:49:06 +0530

    Implemented doubly linked list

---
+++ LinkedList/DoublyLinkedList.py
@@ -0,0 +1,141 @@
+class HandleError:
+  @staticmethod
+  def check_if_value_is_none(value):
+    if value is None:
+      raise Exception("value cannot be none")
+
+  @staticmethod
+  def check_if_index_is_in_range(index:int, start, end):
+    if index < start or index > end:
+      raise Exception("value cannot be none")
+
+class Node:
+  def __init__(self, value):
+    HandleError.check_if_value_is_none(value)
+
+    self.data = value
+    self.next = None
+    self.prev = None
+
+class DoublyLinkedList:
+  def __init__(self):
+    self.head  = None
+    self.length = 0
+  
+  def insert(self, value, index:int = -1):
+    HandleError.check_if_value_is_none(value)
+    HandleError.check_if_index_is_in_range(index, -1, self.length-1)
+
+    if index == 0:
+      self.__insert_at_start(value)
+    elif index == -1:
+      self.__insert_at_end(value)
+    else:
+      self.__insert_at_index(value, index)
+    self.length += 1
+  
+  def delete(self, index:int = -1):
+    HandleError.check_if_index_is_in_range(index, -1, self.length-1)
+    
+    if index == 0:
+      self.__delete_at_start()
+    elif index == -1:
+      self.__delete_at_end()
+    else:
+      self.__delete_at_index(index)
+    self.length -= 1
+  
+  def search(self, value):
+    index = 0
+    current_node = self.head
+    while current_node is not None:
+      if current_node.data == value:
+        return index
+      current_node = current_node.next
+      index += 1
+    return -1
+
+  def display(self):
+    current_node = self.head
+    while current_node is not None:
+      print(current_node.data, end=" <--> ")
+      current_node = current_node.next
+    print("None")
+  
+  def __insert_at_start(self, value):
+    if self.head is None:
+      self.head = Node(value)
+    else:
+      current_head = self.head
+      self.head = Node(value)
+      self.head.next = current_head
+      current_head.prev = self.head
+  
+  def __insert_at_end(self, value):
+    if self.head is None:
+      self.head = Node(value)
+    else:
+      last_node = self.__iterate_to_index(self.length-1)
+      last_node.next  = Node(value)
+      last_node.next.prev = last_node
+  
+  def __insert_at_index(self, value, index):
+    previous_node = self.__iterate_to_index(index-1)
+    next_node = previous_node.next
+    previous_node.next = Node(value)
+    previous_node.next.next = next_node
+    previous_node.next.prev = previous_node
+    next_node.prev = previous_node.next
+
+  def __delete_at_start(self):
+    if self.head is not None:
+      self.head = self.head.next
+      if self.head is not None:
+        self.head.prev = None
+
+  def __delete_at_end(self):
+    if self.head is not None:
+      if self.head.next is None:
+        self.__delete_at_start()
+      else:
+        second_last_node = self.__iterate_to_index(self.length-2)
+        second_last_node.next = None
+
+  def __delete_at_index(self, index):
+    if index == self.length -1:
+      self.__delete_at_end()
+    else:
+      second_last_node = self.__iterate_to_index(index-1)
+      next_node = second_last_node.next.next
+      second_last_node.next = next_node
+      next_node.prev = second_last_node
+  
+  def __iterate_to_index(self, index) -> Node:
+    current_node = self.head
+    for _ in range(1, index+1):
+      current_node = current_node.next
+    return current_node
+
+ll = DoublyLinkedList()
+
+ll.insert(5)
+ll.display() # 5 <--> None
+ll.insert(10, 0)
+ll.display() # 10 <--> 5 <--> None
+ll.insert(15, 1)
+ll.display() # 10 <--> 15 <--> 5 <--> None
+ll.insert(20)
+ll.display() # 10 <--> 15 <--> 5 <--> 20 <--> None
+
+print("Length:", ll.length) # Length: 4
+print(ll.search(15)) # 1
+print(ll.search(16)) # -1
+
+ll.delete(1)
+ll.display() # 10 <--> 5 <--> 20 <--> None
+ll.delete(1)
+ll.display() # 10 <--> 20 <--> None
+ll.delete()
+ll.display() # 10 <--> None
+ll.delete(0)
+ll.display() # None
--- LinkedList/SinglyLinkedList.py
+++ LinkedList/SinglyLinkedList.py
@@ -1,17 +1,17 @@
 class HandleError:
   @staticmethod
-  def check_value_if_none_or_empty(value):
+  def check_value_if_none(value):
     if value is None:
       raise Exception("Value cannot be none or empty")
-  
+
   @staticmethod
   def check_if_index_in_range(index, start, end):
-    if index < start or index >= end:
-      raise Exception("Index should be in range -1 to total length")
+    if index < start or index > end:
+      raise Exception("Index should be in range -1 to total length-1")
 
 class Node:
   def __init__(self, value):
-    HandleError.check_value_if_none_or_empty(value)
+    HandleError.check_value_if_none(value)
 
     self.value = value
     self.next = None
@@ -20,13 +20,13 @@ class SinglyLinkedList:
   def __init__(self):
     self.head = None
     self.length = 0
-  
+
   def insert(self, value, index = -1):
-    HandleError.check_value_if_none_or_empty(value)
+    HandleError.check_value_if_none(value)
     HandleError.check_if_index_in_range(index, -1, self.length)
-    
+
     if index == 0:
-      self.__insert_at_begin(value)
+      self.__insert_at_start(value)
     elif index == -1:
       self.__insert_at_end(value)
     else:
@@ -34,7 +34,7 @@ class SinglyLinkedList:
     self.length += 1
 
   def search(self, value):
-    HandleError.check_value_if_none_or_empty(value)
+    HandleError.check_value_if_none(value)
 
     index = 0
     current_node = self.head
@@ -44,18 +44,18 @@ class SinglyLinkedList:
       current_node = current_node.next
       index += 1
     return -1
-  
+
   def delete(self, index:int =-1):
-    HandleError.check_if_index_in_range(index, -1, self.length)
-    
+    HandleError.check_if_index_in_range(index, -1, self.length-1)
+
     if index == 0:
-      self.__delete_at_begin()
+      self.__delete_at_start()
     elif index == -1:
       self.__delete_at_end()
     else:
       self.__delete_at_index(index)
     self.length -= 1
-  
+
   def display(self):
     current_node = self.head
     while current_node is not None:
@@ -63,43 +63,44 @@ class SinglyLinkedList:
       current_node = current_node.next
     print("None")
 
-  def __insert_at_begin(self, value):
+  def __insert_at_start(self, value):
     if self.head is None:
       self.head = Node(value)
     else:
       current_head = self.head
       self.head = Node(value)
       self.head.next = current_head
-  
+
   def __insert_at_end(self, value):
     if self.head is None:
-      self.__insert_at_begin(value)
+      self.__insert_at_start(value)
     else:
       current_node = self.head
       while current_node.next is not None:
         current_node = current_node.next
       current_node.next = Node(value)
-  
+
   def __insert_at_index(self, value, index):
     current_node = self.head
-    for counter in range(1, index):
+    for _ in range(1, index):
       current_node = current_node.next
     next_node = current_node.next
     current_node.next = Node(value)
     current_node.next.next = next_node
 
-  def __delete_at_begin(self):
-    self.head = self.head.next
-  
+  def __delete_at_start(self):
+    if self.head is not None:
+      self.head = self.head.next
+
   def __delete_at_end(self):
     current_node = self.head
-    while current_node.next.next is not None:
+    while current_node.next is not None and current_node.next.next is not None:
       current_node = current_node.next
     current_node.next = None
-  
+
   def __delete_at_index(self, index):
     current_node = self.head
-    for counter in range(1, index):
+    for _ in range(1, index):
       current_node = current_node.next
     current_node.next = current_node.next.next
     
@@ -115,6 +116,7 @@ ll.display() # 10 --> 15 --> 5 --> None
 ll.insert(20)
 ll.display() # 10 --> 15 --> 5 --> 20 --> None
 
+print("Length:", ll.length) # Length: 4
 print(ll.search(15)) # 1
 print(ll.search(16)) # -1
 
